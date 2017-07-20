d977142810e67d9a9b6742c311babd237ec8a8c4
Adrien Guillo <adrien.guillo@gmail.com>
2017-07-20 14:45:36 -0400

    Initialize page free list

--- kern/pmap.c
+++ kern/pmap.c
@@ -258,10 +258,23 @@ page_init(void)
 	// NB: DO NOT actually touch the physical memory corresponding to
 	// free pages!
 	size_t i;
-	for (i = 0; i < npages; i++) {
-		pages[i].pp_ref = 0;
-		pages[i].pp_link = page_free_list;
-		page_free_list = &pages[i];
+	uint32_t offset;
+
+	uint32_t nextfree = (uint32_t) PADDR(boot_alloc(0));
+
+	pages[0].pp_ref = 1;
+
+	for (i = 1; i < npages; i++) {
+		offset = i * PGSIZE;
+
+		if (offset < npages_basemem * PGSIZE || (offset >= nextfree && offset < KERNBASE)) {
+			pages[i].pp_ref = 0;
+			pages[i].pp_link = page_free_list;
+			page_free_list = &pages[i];
+		}
+		else {
+			pages[i].pp_ref = 1;
+		}
 	}
 }
 
