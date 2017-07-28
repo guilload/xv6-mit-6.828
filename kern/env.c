24d053ef42bb9a004683c8cd0a27301169fd5ea1
Adrien Guillo <adrien.guillo@gmail.com>
2017-07-28 17:44:45 -0400

    Implement `env_run`

--- kern/env.c
+++ kern/env.c
@@ -523,7 +523,15 @@ env_run(struct Env *e)
 	//	e->env_tf to sensible values.
 
 	// LAB 3: Your code here.
+	if (curenv != NULL && curenv->env_status == ENV_RUNNING)
+		curenv->env_status = ENV_RUNNABLE;
 
-	panic("env_run not yet implemented");
+	curenv = e;
+	curenv->env_runs++;
+	curenv->env_status = ENV_RUNNING;
+
+	lcr3(PADDR(curenv->env_pgdir));
+
+	env_pop_tf(&(curenv->env_tf));
 }
 
